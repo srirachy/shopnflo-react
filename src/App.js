824806c6c9c4d55fe31f154159486a5222e03a89
srirachy <caseyereyes@gmail.com>
2023-12-24 18:03:42 -0800

    add initial code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+    return ( 
+        <>
+            <h1>Yo</h1>
+        </>
+     );
+}
+ 
+export default App;
